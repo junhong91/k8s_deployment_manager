c1e6e4c34def4baac482b9417b5b63611e292afa
junhong91 <junhong199@gmail.com>
2022-06-20 22:05:04 +0900

    Autogenerated

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	starlingxv1 "github.com/junhong91/k8s_deployment_manager/api/v1"
+)
 
 var v int32
 
 func main() {
 	fmt.Println(v)
+
+	d := starlingxv1.DataNetwork{}
+	fmt.Println(d)
 }
 
 // init() function called when package loaded at first
