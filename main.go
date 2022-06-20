002280f96c3a80a6afcb17c11d94be1941f349cc
junhong91 <junhong199@gmail.com>
2022-06-20 21:40:37 +0900

    Init function called whenever package loaded

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+var v int32
+
 func main() {
-	fmt.Println("TEST")
+	fmt.Println(v)
+}
+
+// init() function called when package loaded at first
+func init() {
+	v = 1
 }
